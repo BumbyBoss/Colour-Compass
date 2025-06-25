77f09230836bc3e50946d2c747965a540001bf7d
BumbyBoss <94413243+BumbyBoss@users.noreply.github.com>
2025-06-25 13:13:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,57 @@ let swatches = [];
 let selectedColours = {};
 
 const layoutMap = {
-  // ... already present structure from canvas ...
+  grid: [
+    "zone-1", "zone-2", "zone-3",
+    "zone-4", "zone-5", "zone-6",
+    "zone-7", "zone-8", "zone-9"
+  ],
+  diaper: {
+    Abrazo: ["AZBody", "AZLeftTab", "AZRightTab"],
+    TraditionalDC: ["TDCBody", "TDCWaistband", "TDCLeftCuff", "TDCRightCuff"],
+    Brief: ["BriefBody", "BriefWaistband", "BriefLeftCuff", "BriefRightCuff"]
+  },
+  kidpants: {
+    HaremPants: ["HPBody", "HPWaist", "HPLeftCuff", "HPRightCuff"],
+    LeggingPants: ["LPBody", "LPWaist"],
+    JoggersPants: ["JPBody", "JPWaist", "JPLeftCuff", "JPRightCuff"],
+    RufflePants: ["RPBody", "RPWaist"],
+    HemmedPants: ["HPBody", "HPWaist"],
+    CuffedPants: ["CPBody", "CPWaist", "CPLeftCuff", "CPRightCuff"],
+    EuroHemmedPants: ["EHBody", "EHWaist"],
+    EuroCuffedPants: ["ECBody", "ECWaist", "ECLeftCuff", "ECRightCuff"],
+    EuroSweatPants: ["ESPBody", "ESPWaist", "ESPLeftCuff", "ESPRightCuff"],
+    BritchesPants: ["BPBody", "BPWaist", "BPLeftLeg", "BPRightLeg"],
+    FootiesPants: ["FPBody", "FPWaist", "FPLeftFoot", "FPRightFoot"],
+    BootcutPants: ["BCPBody", "BCPWaist"]
+  },
+  kidcrops: {
+    HemmedCrop: ["HCBody", "HCWaist"],
+    HemmedEuroCrop: ["HECBody", "HECWaist"],
+    DigsCrop: ["DigsBody", "DigsWaist", "DigsLeftCuff", "DigsRightCuff"],
+    CuffedCrop: ["CCBody", "CCWaist", "CCLeftCuff", "CCRightCuff"],
+    CuffedEuroCrop: ["CECBody", "CECWaist", "CECLeftCuff", "CECRightCuff"],
+    RuffleCrop: ["RCBody", "RCWaist"]
+  },
+  kidshorts: {
+    HemmedShorts: ["HSBody", "HSWaist"],
+    CuffedShorts: ["CSBody", "CSWaist", "Cuffs"],
+    RuffledShorts: ["RSBody", "RSWaist"],
+    BloomerShorts: ["BSBody", "BSWaist", "Cuffs"],
+    Skirtie: ["SkBody", "SkWaist"]
+  },
+  sweater: {
+    CampfireSweater: ["CampfireBody", "CampfireLeftSleeve", "CampfireRightSleeve", "CampfireLeftCuff", "CampfireRightCuff", "CampfireBottomCuff", "CampfireNeck"],
+    CardiganSweater: ["CardiganSweaterBody", "CardiganLeftSleeve", "CardiganRightSleeve", "CardiganLeftCuff", "CardiganRightCuff", "CardiganBottomCuff", "Neck"],
+    CocoonSweater: ["CocoonBody", "CocoonLeftSleeve", "CocoonRightSleeve", "CocoonLeftCuff", "CocoonRightCuff", "CocoonTrim"],
+    CrewNeckSweater: ["CrewNeckSweaterBody", "CrewNeckLeftSleeve", "CrewNeckRightSleeve", "CrewNeckLeftCuff", "CrewNeckRightCuff", "CrewNeckBottomCuff", "CrewNeck"],
+    HalfZipSweater: ["HalfZipSweaterBody", "HalfZipLeftSleeve", "HalfZipRightSleeve", "HalfZipLeftCuff", "HalfZipRightCuff", "HalfZipBottomCuff", "Neck"],
+    HenleySweater: ["HenleySweaterBody", "HenleyLeftSleeve", "HenleyRightSleeve", "HenleyLeftCuff", "HenleyRightCuff", "HenleyBottomCuff", "Neck"],
+    ShawlNeckSweater: ["ShawlSweaterBody", "ShawlLeftSleeve", "ShawlRightSleeve", "ShawlLeftCuff", "ShawlRightCuff", "ShawlBottomCuff", "ShawlTrim"],
+    SheepyHugSweater: ["SheepySweaterBody", "SheepyLeftSleeve", "SheepyRightSleeve", "SheepyLeftCuff", "SheepyRightCuff", "SheepyBottomCuff", "SheepyHugHood"],
+    QuarterSweater: ["QuarterSweaterBody", "QuarterLeftSleeve", "QuarterRightSleeve", "QuarterLeftCuff", "QuarterRightCuff", "QuarterBottomCuff", "Neck"],
+    Vest: ["VestChest", "VestLeftSidePanel", "VestRightSidePanel", "VestLowerTorso", "VestBottomCuff", "Neck"]
+  }
 };
 
 const layoutSelect = document.getElementById("layout");
